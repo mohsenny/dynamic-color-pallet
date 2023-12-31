dae0855b3512b09f70a90f33e276ce401dac7ce6
unknown <mohsen.n89@gmail.com>
2023-12-31 01:20:03 +0100

    increase strip height

--- src/modules/horizontalStrip.js
+++ src/modules/horizontalStrip.js
@@ -1,6 +1,6 @@
 const drawHorizontalStrip = (colors, ctx, x, y) => {
   const colorWidth = 300 / colors.length; // Assuming a fixed width for the strip
-  const height = 50; // Fixed height for the strip
+  const height = 100; // Fixed height for the strip
 
   for (let index = 0; index < colors.length; index++) {
     ctx.fillStyle = colors[index];
