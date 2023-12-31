91ba3c9bf6d64412532b3a979360823a59ef6bed
unknown <mohsen.n89@gmail.com>
2023-12-31 01:00:12 +0100

    Introduce more variety into hue and brightness diversity

--- src/utils/colorReader.js
+++ src/utils/colorReader.js
@@ -1,43 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
-// const generateRandomColors = (numColors) => {
-//   const randomColors = [];
-//   for (let i = 0; i < numColors; i++) {
-//     const color = `#${Math.floor(Math.random() * 16777215)
-//       .toString(16)
-//       .padEnd(6, "0")}`;
-//     randomColors.push(color);
-//   }
-//   return randomColors;
-// };
-
-const generateSplitComplementaryColors = (numColors) => {
-  // Generate a random base hue between 0 and 360
+const generateUXFriendlyColors = (numColors) => {
+  // Generate a random base hue for each palette
   const baseHue = Math.floor(Math.random() * 360);
-  
-  // Define a base saturation and lightness value
-  // Avoiding too high or too low values that result in greys or overly vibrant colors
-  const baseSaturation = Math.floor(Math.random() * 50) + 30; // Saturation between 30% and 80%
-  const baseLightness = Math.floor(Math.random() * 40) + 30; // Lightness between 30% and 70%
+  const hueIncrement = Math.floor(360 / numColors); // Ensures a wide range of hues
 
-  let colors = [`hsl(${baseHue}, ${baseSaturation}%, ${baseLightness}%)`];
+  let colors = [];
 
-  // Calculate the split complementary hues
-  const splitHue1 = (baseHue + 150) % 360;
-  const splitHue2 = (baseHue + 210) % 360;
+  for (let i = 0; i < numColors; i++) {
+    // Adjust hue for each color to ensure variety
+    const currentHue = (baseHue + i * hueIncrement) % 360;
+    
+    let adjustSaturation, adjustLightness;
 
-  colors.push(`hsl(${splitHue1}, ${baseSaturation}%, ${baseLightness}%)`);
-  colors.push(`hsl(${splitHue2}, ${baseSaturation}%, ${baseLightness}%)`);
+    // Alternate between different saturation and lightness for contrast
+    if (i % 2 === 0) {
+      // More saturated and darker for contrast
+      adjustSaturation = 70 + Math.floor(Math.random() * 30); // 70% to 100%
+      adjustLightness = 20 + Math.floor(Math.random() * 40); // 20% to 60%
+    } else {
+      // Less saturated and lighter
+      adjustSaturation = 30 + Math.floor(Math.random() * 40); // 30% to 70%
+      adjustLightness = 60 + Math.floor(Math.random() * 40); // 60% to 100%
+    }
 
-  // Generate additional colors by adjusting saturation and lightness
-  for (let i = 3; i < numColors; i++) {
-    // Adjust the base values to get a variety of colors
-    const adjustSaturation = (baseSaturation + 20 * (i % 2)) % 100;
-    const adjustLightness = (baseLightness + 20 * ((i + 1) % 2)) % 100;
-
-    const newHue = (baseHue + i * 30) % 360;
-    colors.push(`hsl(${newHue}, ${adjustSaturation}%, ${adjustLightness}%)`);
+    colors.push(`hsl(${currentHue}, ${adjustSaturation}%, ${adjustLightness}%)`);
   }
 
   return colors;
@@ -50,8 +38,7 @@ const readColorsFromFile = () => {
 };
 
 const getColors = (useRandomColors = false, numColors = 5) => {
-  return useRandomColors ? generateSplitComplementaryColors(numColors) : readColorsFromFile();
-
+  return useRandomColors ? generateUXFriendlyColors(numColors) : readColorsFromFile();
 };
 
 module.exports = getColors;
